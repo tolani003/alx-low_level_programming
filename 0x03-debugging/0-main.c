39ba61aca8e79dd5a71027e68bdf3f5e51f338ec
tolani <tolanionajoko2003@gmail.com>
2023-07-18 17:40:54 -0700

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,20 +11,3 @@ int main(void)
     positive_or_negative(i);
     return (0);
 }
-
-/**
- * positive_or_negative - Checks if a number is positive or negative
- * @n: The number to check
- *
- * Return: void
- */
-void positive_or_negative(int n)
-{
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n < 0)
-        printf("%d is negative\n", n);
-    else
-        printf("%d is zero\n", n);
-}
-
