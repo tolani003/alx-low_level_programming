ba9ca34ce19c31264fe97b525f33d9fd78b7dee3
tolani <tolanionajoko2003@gmail.com>
2023-07-27 16:00:49 -0700

    ./100-rot13

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - copy a string
  * @dest: input value
@@ -6,7 +7,8 @@
  * @n: input value
  *
  * Return: dest
- */
+*/
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
